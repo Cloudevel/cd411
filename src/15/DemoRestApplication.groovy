f64a8889b63550dbd4084380ac8e02132f4432a6
Jose Luis Chiquete <josech@gmail.com>
2020-04-14 05:41:29 -0500

    200414a

---
+++ src/15/DemoRestApplication.groovy
@@ -0,0 +1,13 @@
+package com.cloudevel.demorest
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoRestApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DemoRestApplication, args)
+	}
+
+}
---
+++ src/15/ProductServiceController.java
@@ -0,0 +1,55 @@
+package com.cloudevel.demorest.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cloudevel.demorest.model.Product;
+
+@RestController
+public class ProductServiceController {
+   private static Map<String, Product> productRepo = new HashMap<>();
+   static {
+      Product leche = new Product();
+      leche.setId("1");
+      leche.setName("Leche");
+      productRepo.put(leche.getId(), leche);
+      
+      Product pan = new Product();
+      pan.setId("2");
+      pan.setName("Pan de caja");
+      productRepo.put(pan.getId(), pan);
+   }
+   
+   @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+   public ResponseEntity<Object> delete(@PathVariable("id") String id) { 
+      productRepo.remove(id);
+      return new ResponseEntity<>("Producto eliminado.", HttpStatus.OK);
+   }
+   
+   @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+   public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product) { 
+      productRepo.remove(id);
+      product.setId(id);
+      productRepo.put(id, product);
+      return new ResponseEntity<>("Producto actualizado.", HttpStatus.OK);
+   }
+   
+   @RequestMapping(value = "/products", method = RequestMethod.POST)
+   public ResponseEntity<Object> createProduct(@RequestBody Product product) {
+      productRepo.put(product.getId(), product);
+      return new ResponseEntity<>("Producto creado.", HttpStatus.CREATED);
+   }
+   
+   @RequestMapping(value = "/products")
+   public ResponseEntity<Object> getProduct() {
+      return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+   }
+}
